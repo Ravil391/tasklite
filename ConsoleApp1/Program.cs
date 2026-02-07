6c5389fd62143fa7b5328b15bc47498e59ef1d8a
Ravil391 <gamesszedlak@gmail.com>
2026-02-07 18:39:14 +0300

    style(task): кнопки действий - базовые стили и состояния

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace MetroStationNavigatorTask 
+{
+    public class MetroStationNavigatorTestsTask
+    {
+        private IMetroStationNavigator _navigator;
+        public virtual IMetroStationNavigator CreateNavigator() => new MetroStationNavigator(); // это удалять нельзя
+
+        [TestCase("1", "1", "1", "return", TestName = "Минимальные значение параметров")]
+        public void TestPath(string hullIntegrity, string shieldCharge, string reactorStability, string result)
+        {
+            _navigator = CreateNavigator();
+            result.Should().BeEquivalentTo(_navigator.GetPath(hullIntegrity, shieldCharge, reactorStability));
+        }
+    }
+}
--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let difference = a - b;
 console.log(sum);
 console.log(difference);
 
-// Урок: операторы и условия //
+// Урок:
+//  операторы и условия //
 
 let title = "";
 if(title === ""){
@@ -45,4 +46,38 @@ function TaskSummary(total, done){
 console.log(taskSummary(5,4));
 console.log(10,4);
 
+let cities = ["Москва", "Париж", "Питер", "Токио"];
+cities[2] = "Лондон";
+console.log(cities);
 
+let task = {
+id: 1,
+title: "Купить молоко",
+status: "активна"
+};
+console.log(task.id);
+console.log(task.title);
+console.log(task.status);
+
+let tasks = [
+    {id: 1, title: "Купить молоко", status: "активна"},
+    {id: 2, title: "Позвонить врачу", status: "выполнена"},
+    {id: 3, title: "Купить молоко", status: "активна" },
+];
+
+console.log(tasks[0].title);
+console.log(tasks[1].status);
+
+tasks[0].status = "выполнена";
+console.log(task[0]);
+
+tasks.push({id: 4, title: "Прогулка", status: "активна"});
+console.log(tasks);
+
+let user = {
+    name: "Анна",
+    tasks: tasks
+};
+
+console.log(user.name);
+console.log(user.tasks.length);
